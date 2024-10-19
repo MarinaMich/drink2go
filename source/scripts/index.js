cf2296ed826853b8792ab6e188fb202260fd7a3a
MarinaMich <mar7995@yandex.ru>
2024-10-19 19:16:23 +0300

    Добавляет верстку шапки сайта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+/* Выпадающее меню в мобильной версии*/
+let menuMain = document.querySelector('.menu');
+let menuToggle = document.querySelector('.menu__toggle');
+
+menuToggle.addEventListener('click', function () {
+  console.log("клик");
+  if (menuMain.classList.contains('menu--closed')) {
+    menuMain.classList.remove('menu--closed');
+    menuMain.classList.add('menu--opened');
+  } else {
+    menuMain.classList.add('menu--closed');
+    menuMain.classList.remove('menu--opened');
+  }
+});
