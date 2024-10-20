f81324c80dffe936861412b1dc4682f698c69a6d
MarinaMich <mar7995@yandex.ru>
2024-10-20 16:27:06 +0300

    Добавляет функционал слайдеру

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -12,3 +12,39 @@ menuToggle.addEventListener('click', function () {
     menuMain.classList.remove('menu--opened');
   }
 });
+
+/*Слайдер блока promo*/
+const slider = document.querySelector('.slider__list');
+const slides = Array.from(slider.querySelectorAll('.slide'));
+const count = slides.length;
+const nextButton = document.querySelector('.slider__control--next');
+const prevButton = document.querySelector('.slider__control--prev');
+let currentIndex = 0;
+
+const slide = () => {
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0){
+    currentIndex = (currentIndex - 1 + count) % count;
+  } else {
+    prevButton.disabled = true;
+  }
+  slide();
+  prevButton.disabled = false;
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentIndex < count - 1) {
+    currentIndex = (currentIndex + 1) % count;
+} else {
+  nextButton.disabled = true;
+}
+  slide();
+  nextButton.disabled = false;
+});
+
+window.addEventListener('load', () => {
+  slide();
+});
